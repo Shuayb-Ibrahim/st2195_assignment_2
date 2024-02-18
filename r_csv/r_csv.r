0735eb7ca953763bc5b87b0514a617fec09c981c
Shuayb Ibrahim <sibrahimhajd@gmail.com>
2024-02-18 22:55:53 +0000

    python_commit

--- r_csv/r_csv.r
+++ r_csv/r_csv.r
@@ -5,3 +5,6 @@ wiki_cars <- read_html("https://en.wikipedia.org/wiki/Comma-separated_values")
 str(wiki_cars)
 
 popnodes <- html_nodes(wiki_cars, "table")
+pop <- html_table(popnodes, header = TRUE, fill = TRUE)[[2]]
+
+write.csv(pop, file = "r_csv.csv")
